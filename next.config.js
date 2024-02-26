63cfe2255e83a7d5211f0e70589bb636896c1100
kentur <kevinxrturop@gmail.com>
2024-02-26 12:19:07 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ module.exports = {
     images: {
         domains: ['stackfood.6am.one'],
     },
-    staticPageGenerationTimeout: 220, // Aumentar el tiempo de espera a 120 segundos (o ajusta según sea necesario)
+    staticPageGenerationTimeout: 320, // Aumentar el tiempo de espera a 120 segundos (o ajusta según sea necesario)
 }
