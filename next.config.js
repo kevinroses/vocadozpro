f5fbcfcb624532d5f59a94fcbd50330cf8bd7419
kentur <kevinxrturop@gmail.com>
2024-02-26 10:50:08 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-
-
+// next.config.js
 module.exports = {
     reactStrictMode: true,
     images: {
         domains: ['stackfood.6am.one'],
-       // staticPageGenerationTimeout: 1500//Domain of image host
     },
+    staticPageGenerationTimeout: 120, // Aumentar el tiempo de espera a 120 segundos (o ajusta según sea necesario)
 }
