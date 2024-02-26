5064bc752ef87f78d0c3af2fc53690c2bf119a45
kentur <kevinxrturop@gmail.com>
2024-02-26 16:43:04 -0500

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ module.exports = {
     images: {
         domains: ['stackfood.6am.one'],
     },
-    staticPageGenerationTimeout: 320, // Aumentar el tiempo de espera a 120 segundos (o ajusta según sea necesario)
+    staticPageGenerationTimeout: 60, // Aumentar el tiempo de espera a 120 segundos (o ajusta según sea necesario)
 }
